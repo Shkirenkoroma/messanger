50cb93deb13997bc39e0d27b53a3b4c611936e26
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-02-26 02:05:56 +0300

    added new feature

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -10,112 +10,113 @@ interface IItem {
 }
 
 const Container = styled.div`
-	width: 100px;
-	height: 100px;
+	display: flex;
+	width: 100%;
+	div.aside {
+		max-width: 240px;
+		width: 100%;
+		background-color: #091336;
+		div.aside__container {
+		}
+	}
+	div.main {
+		width: 100%;
+	}
 	header {
 		background-color: #ffffff;
 		height: 65px;
-		width: 100vw;
+		width: 100%;
 	}
 	main {
 		background-color: blue;
 		height: calc(100vh - 65px);
-		width: 100vw;
-	}
-	aside {
-		background-color: #091336;
-		position: absolute;
-		top: 0;
-		left: 0;
-		height: 100vh;
-		max-width: 240px;
 		width: 100%;
-		div {
-			width: 100%;
-			img#logo {
-				margin:20px 0 32px 12px;
-			}
-			div#container__content {
-				margin-bottom: 64px;
-				div#container__content__item {
-					display: flex;
-					align-items: center;
-					max-width: 240px;
-					width: 100%;
-					height: 52px;
-					&:hover {
-						img#icon {
-							fill-opacity:1;
-						}
-						span {
-							color: #ffffff;
-						}
-						cursor: pointer;
-						background-color: #cdcdcd;
-						span#cicle {
-							background-color: #ffd500;
-						}
-						span#menu__side {
-							background-color: #005ff8;
-						}
-					}
-					span#menu__side {
-						height: 50px;
-						width: 3px;
-						background-color: transparent;
-						display: block;
-						margin-right: 12px;
-					}
-					img {
-						margin-right: 13px;
-					}
+	}
+	div {
+		width: 100%;
+		img.logo {
+			margin: 20px 0 32px 12px;
+		}
+		div.container__content {
+			margin-bottom: 64px;
+			position: relative;
+			div.container__content__item {
+				display: flex;
+				align-items: center;
+				max-width: 240px;
+				width: 100%;
+				height: 52px;
+				&:hover {
 					span {
-						font-family: "SF Pro Display";
-						font-style: normal;
-						font-weight: 500;
-						font-size: 16px;
-						line-height: 24px;
-						display: flex;
-						align-items: center;
-						color: rgba(255, 255, 255, 0.6);
+						color: #ffffff;
 					}
-					span#cicle {
-						width: 8px;
-						height: 8px;
-						position: absolute;
-						right: 22px;
-						background-color: transparent;
-						border-radius: 50%;
+					cursor: pointer;
+					background-color: #cdcdcd;
+					transition: ease background-color 700ms;
+					span.cicle {
+						background-color: #ffd500;
+						transition: ease background-color 700ms;
+					}
+					span.menu__side {
+						background-color: #005ff8;
+						transition: ease background-color 700ms;
 					}
 				}
-			}
-			div#buttons{
-				margin-left:12px;
-				display: flex;
-				flex-direction: column;
-				button{
-					max-width:200px;
-					width:100%;
-					background-color: #005FF8;
-					padding: 13px 24px;
+				span.menu__side {
+					height: 50px;
+					width: 3px;
+					background-color: transparent;
+					display: block;
+					margin-right: 12px;
+				}
+				img {
+					margin-right: 13px;
+				}
+				span {
+					font-family: "SF Pro Display";
+					font-style: normal;
+					font-weight: 500;
+					font-size: 16px;
+					line-height: 24px;
 					display: flex;
 					align-items: center;
-					justify-content: space-around;
-					border-radius: 4px;
-					border: none;
-					margin-bottom: 32px;
-					&:hover{
-						cursor: pointer;
-					
-					}
-					span{
-						font-family: 'SF Pro Display';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 148%;
-color: #FFFFFF;
-					}
+					color: rgba(255, 255, 255, 0.6);
+				}
+				span.cicle {
+					width: 8px;
+					height: 8px;
+					position: absolute;
+					right: 22px;
+					background-color: transparent;
+					border-radius: 50%;
+				}
+			}
+		}
+		div.buttons {
+			margin-left: 12px;
+			display: flex;
+			flex-direction: column;
+			button {
+				max-width: 200px;
+				width: 100%;
+				background-color: #005ff8;
+				padding: 13px 24px;
+				display: flex;
+				align-items: center;
+				justify-content: space-around;
+				border-radius: 4px;
+				border: none;
+				margin-bottom: 32px;
+				&:hover {
+					cursor: pointer;
+				}
+				span {
+					font-family: "SF Pro Display";
+					font-style: normal;
+					font-weight: 500;
+					font-size: 16px;
+					line-height: 148%;
+					color: #ffffff;
 				}
 			}
 		}
@@ -125,30 +126,38 @@ color: #FFFFFF;
 const Main = () => {
 	return (
 		<Container>
-			<header>Header</header>
-			<main>
-				<section>section one</section>
-				<section>section two</section>
-			</main>
-			<aside>
-				<div id="container">
-					<img id="logo" src={logo} alt="logo" />
-					<div id="container__content">
+			<div className="aside">
+				<div className="aside__container">
+					<img className="logo" src={logo} alt="logo" />
+					<div className="container__content">
 						{items.map((item: IItem, index: number) => (
-							<div id="container__content__item" key={index}>
-								<span id="menu__side" />
-								<img id="icon" src={item.img} alt={item.alt} />
+							<div className="container__content__item" key={index}>
+								<span className="menu__side" />
+								<img className="icon" src={item.img} alt={item.alt} />
 								<span>{item.describe}</span>
-								<span id="cicle" />
+								<span className="cicle" />
 							</div>
 						))}
 					</div>
-					<div id="buttons">
-					<button><span>Добавить заказ</span><img src={plus} alt="button"/></button>
-					<button><span>Оплата</span><img src={vector} alt="button"/></button>
+					<div className="buttons">
+						<button>
+							<span>Добавить заказ</span>
+							<img src={plus} alt="button" />
+						</button>
+						<button>
+							<span>Оплата</span>
+							<img src={vector} alt="button" />
+						</button>
 					</div>
 				</div>
-			</aside>
+			</div>
+			<div className="main">
+				<header>Header</header>
+				<main>
+					<section>section one</section>
+					<section>section two</section>
+				</main>
+			</div>
 		</Container>
 	);
 };
